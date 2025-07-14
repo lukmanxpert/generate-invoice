68f3d00df4ee9349c9cfe80653f401713970c408
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-14 23:18:42 +0600

    update userprofiledropdown

--- src/app/(dashboard)/_components/UserProfileDropdown.tsx
+++ src/app/(dashboard)/_components/UserProfileDropdown.tsx
@@ -1,25 +1,39 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
+  DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { auth } from "@/lib/auth";
+import getAvatarName from "@/lib/getAvatarName";
+import { ChevronDown } from "lucide-react";
 import React from "react";
 
 export default async function UserProfileDropdown() {
   const session = await auth();
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <div>
-          <Avatar>
+      <DropdownMenuTrigger asChild>
+        <div className="flex items-center gap-3 cursor-pointer">
+          <Avatar className="border size-9 bg-neutral-300">
             <AvatarImage src={session?.user.image as string} />
             <AvatarFallback>
-                SL
+              {getAvatarName(
+                session?.user.firstName as string,
+                session?.user.lastName as string
+              )}
             </AvatarFallback>
           </Avatar>
+          <div>
+            <p>
+              <span>{session?.user.firstName}</span>{" "}
+              <span>{session?.user.lastName}</span>
+            </p>
+          </div>
+          <ChevronDown />
         </div>
       </DropdownMenuTrigger>
+      <DropdownMenuContent>Pop Over</DropdownMenuContent>
     </DropdownMenu>
   );
 }
---
+++ src/lib/getAvatarName.ts
@@ -0,0 +1,9 @@
+export default function getAvatarName(
+  firstName: string,
+  lastName: string | null
+) {
+  if (!firstName) {
+    return null;
+  }
+  return lastName ? firstName[0] + lastName[0] : firstName[0];
+}
