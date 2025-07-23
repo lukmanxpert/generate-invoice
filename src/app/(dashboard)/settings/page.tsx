7b13d01732f4c018628cb1d9853cd82daad7f88b
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-23 16:06:04 +0600

    settings page

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -9,7 +9,8 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import imageBase64 from "@/lib/imageBase64";
 import Image from "next/image";
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 type TSignatureData = {
   name: string;
@@ -17,104 +18,171 @@ type TSignatureData = {
 };
 
 export default function SettingsPage() {
-  const [logo, setLogo] = useState<string>("");
-  const [signature, setSignature] = useState<TSignatureData>({
+  const [logo, setLogo] = useState<string>();
+  const [signatureData, setSignatureData] = useState<TSignatureData>({
     name: "",
     image: "",
   });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const router = useRouter();
 
-  // handle onchange signature: ex.g name
-  const onchangeSignature = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setSignature((prev) => {
-      return { ...prev, [name]: value };
+  //handle on change signature : ex.g name
+  const onChangeSignature = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setSignatureData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
     });
   };
 
-  // handle onchange signature image
+  //handle on change signature image
   const handleSignatureImage = async (
-    event: React.ChangeEvent<HTMLInputElement>
+    e: React.ChangeEvent<HTMLInputElement>
   ) => {
-    const files = event.target.files;
+    const files = e.target.files;
+
     if (!files || files.length < 0) return;
+
     const file = files[0];
 
-    // image to base64
+    //image to base64
     const image = await imageBase64(file);
-    setSignature((prev) => ({ ...prev, image: image }));
+
+    setSignatureData((preve) => {
+      return {
+        ...preve,
+        image: image,
+      };
+    });
   };
 
+  //handle on change logo
+  const handleOnChangeLogo = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+
+    if (!files || files.length < 0) return;
+
+    const file = files[0];
+
+    //image to base64
+    const image = await imageBase64(file);
+
+    setLogo(image);
+  };
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+    data: any
+  ) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const response = await fetch("/api/settings", {
+        method: "post",
+        body: JSON.stringify(data),
+      });
+      if (response.status === 200) {
+        toast.success("Setting updated Successfully");
+        fetchData();
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log("error :>> ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="p-4">
       <div>
         <h1 className="font-semibold text-xl">Settings</h1>
       </div>
-      <Accordion type="multiple">
-        {/* invoice logo */}
-        <AccordionItem value="invoice-logo">
+
+      <Accordion type="single">
+        {/**Invoice Logo */}
+        <AccordionItem value="Invoice-Logo">
           <AccordionTrigger className="font-semibold text-base cursor-pointer">
             Invoice Logo
           </AccordionTrigger>
           <AccordionContent>
-            <form className="w-full grid gap-2">
-              <Input type="file" className="max-w-sm w-full" />
-              <div className="w-full max-w-sm">
-                {signature.image ? (
+            <form
+              className="w-full grid gap-2"
+              onSubmit={(e) => handleSubmit(e, { logo })}
+            >
+              <Input
+                type="file"
+                className="max-w-sm w-full"
+                onChange={handleOnChangeLogo}
+                required
+                disabled={isLoading}
+              />
+              <div className="w-full max-w-xs">
+                {logo ? (
                   <Image
-                    className="aspect-square h-20 border-2 border-dotted max-h-20 object-scale-down"
-                    src={signature.image}
-                    alt="signature sign"
-                    width={205}
+                    className="aspect-video h-20 border-2 border-dotted max-h-20 object-scale-down"
+                    src={logo}
+                    width={250}
                     height={96}
+                    alt="Invoice logo"
                   />
                 ) : (
                   <div className="aspect-video h-20 border-2 border-dotted flex justify-center items-center rounded-lg">
-                    <p className="text-center text-muted-foreground">
-                      No logo selected
-                    </p>
+                    <p className="text-center text-muted-foreground">No Data</p>
                   </div>
                 )}
               </div>
-              <Button variant={"outline"} className="w-fit cursor-pointer">
-                Save
+              <Button className="w-fit" disabled={isLoading}>
+                {isLoading ? "Please wait..." : "save"}
               </Button>
             </form>
           </AccordionContent>
         </AccordionItem>
-        {/* signature in invoice */}
-        <AccordionItem value="Signature Invoice">
+
+        {/***Signature in invoice */}
+        <AccordionItem value="Signature-invoice">
           <AccordionTrigger className="font-semibold text-base cursor-pointer">
-            Signature Invoice
+            {" "}
+            Invoice Signature
           </AccordionTrigger>
           <AccordionContent>
-            <form className="w-full grid gap-2">
+            <form
+              className="w-full grid gap-2"
+              onSubmit={(e) => handleSubmit(e, { signature: signatureData })}
+            >
               <Input
                 type="text"
                 placeholder="Enter your signature name"
-                value={signature.name}
-                onChange={onchangeSignature}
+                value={signatureData.name}
+                onChange={onChangeSignature}
                 name="name"
+                disabled={isLoading}
               />
-              {/* <Input type="file" className="max-w-sm w-full" /> */}
-              <div className="w-full max-w-sm">
-                {logo ? (
+              <Input
+                type="file"
+                className="max-w-sm w-full"
+                onChange={handleSignatureImage}
+                disabled={isLoading}
+              />
+              <div className="w-full max-w-xs">
+                {signatureData.image ? (
                   <Image
-                    className="aspect-square h-20 border-2 border-dotted max-h-20 object-scale-down"
-                    src={logo}
-                    alt="invoice logo"
-                    width={205}
+                    className="aspect-video h-20 border-2 border-dotted max-h-20 object-scale-down"
+                    src={signatureData.image}
+                    width={250}
                     height={96}
+                    alt="Signature sign"
                   />
                 ) : (
                   <div className="aspect-video h-20 border-2 border-dotted flex justify-center items-center rounded-lg">
-                    <p className="text-center text-muted-foreground">
-                      No logo selected
-                    </p>
+                    <p className="text-center text-muted-foreground">No Data</p>
                   </div>
                 )}
               </div>
-              <Button variant={"outline"} className="w-fit cursor-pointer">
-                Save
+              <Button className="w-fit" disabled={isLoading}>
+                {isLoading ? "Please wait..." : "save"}
               </Button>
             </form>
           </AccordionContent>
