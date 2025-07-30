53e00cfbe5a1cd410ca5305071f4dd47ce678a6a
lukmanxpert <lukmanmiah2004@gmail.com>
2025-07-30 17:13:50 +0600

    added userInvoiceSchma and itemSchema

--- src/models/invoice.model.ts
+++ src/models/invoice.model.ts
@@ -41,3 +41,28 @@ export interface IInvoice {
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+const userInvoiceSchema = new mongoose.Schema<IUserInvoice>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    address1: { type: String, required: true },
+    address2: { type: String, default: null },
+    address3: { type: String, default: null },
+  },
+  {
+    _id: false,
+  }
+);
+
+const itemSchema = new mongoose.Schema<IItems>(
+  {
+    item_name: { type: String, default: null, required: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    total: { type: Number, required: true },
+  },
+  {
+    _id: false,
+  }
+);
