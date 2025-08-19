4e41d06407b5fc2d14e65d25f55cf4f027f0a369
lukmanxpert <lukmanmiah2004@gmail.com>
2025-08-20 01:16:28 +0600

    modifyed dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,20 +1,23 @@
 import ProtectedPage from "@/components/CheckAuth";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { signOut } from "@/lib/auth";
 import React from "react";
 
 export default function DashboardPage() {
   return (
-    <div>
-      Dashboard
-      <Button
-        onClick={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
-        Logout
-      </Button>
+    <div className="p-4 grid gap-6 grid-cols-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Total Revenue</CardTitle>
+        </CardHeader>
+        <CardContent className="py-0">
+          <div>
+            <p className="text-lg">$400</p>
+            <span className="text-muted-foreground text-xs">last 30 days</span>
+          </div>
+        </CardContent>
+      </Card>
       <ProtectedPage />
     </div>
   );
